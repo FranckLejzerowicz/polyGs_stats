055d36c897d84ca50a93e3e2626ab2e7cacc1528
FranckLejzerowicz <franck.lejzerowicz@unige.ch>
2020-02-04 14:30:47 -0800

    inits

---
+++ build/lib/script/__init__.py
@@ -0,0 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2020, Franck Lejzerowicz.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
+from ._polyGs_stats_dummy import polyGs_stats_dummy
+
+__all__ = ["polyGs_stats_dummy"]
